9ac3ea3d478b10e5b16da4fad0b9079e02d7bc8c
Messias Daniel <messiasdaniel1999@gmail.com>
2025-06-14 12:16:24 -0300

    subindo aula 07

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ function verificarNumero() {
 let numero = document.getElementById("numero").value;
 numero = Number(numero);
 let mensagem = "";
-if (numero < 0){
-    mensagem = "O Número é positivo";
+
+if (numero < 0) {
+    mensagem = "O Número é negativo";
 }
 else if (numero > 0){
-    mensagem = "O número é negativo";}
-    
-    Document.getElementById("Resultado").innertext = mensagem;
+    mensagem = "O número é positivo";
 }
+ else {  
+    mensagem = "o número é zero"
+ }   
+    document.getElementById("resultado").innerText= mensagem;
+}
+
+
+
+
+function executaFor() {
+ let resultado = "Números pares de 0 a 10: ";
+for (let i = 0; i <= 10; i++) {
+    if(i % 2 === 0) {
+        resultado += i + " ";
+    }
+       
+}
+document.getElementById("saida").textContent = resultado;
+}
+
+    function executarArray() {
+            let nomes = ["Manu", "Samira", "Andreia", "Ana"];
+            let resultado = "Lista de alunas:" ;
+
+            for (let i = 0; nomes.length; i++) {
+                resultado += nomes [i]
+            }
+
+    document.getElementById("saida").textContent = resultado;
+        }
